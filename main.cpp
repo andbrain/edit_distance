41ebc099360cc854a86b3ff59b71729c7fe195fe
Anderson Pimentel <andbrain@gmail.com>
2019-04-26 15:28:35 -0400

    chore: change sample name

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "editDistance.h"
 
 int main(int argc, char const *argv[]) {
-  ed_classic::editDistance ed("abcd","xad", 1);
+  ed_classic::editDistance ed("jon","johnny", 1);
   std::cout << ed.process() << '\n';
 
   return 0;
